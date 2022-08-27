faaf606c01ad300a42e689ded5caa61c9438d114
Sarah Hesham <sarahheshammahmoud@gmail.com>
2022-08-27 22:11:46 +0200

    Add files via upload

---
+++ Graphics Bezier Curves/main.cpp
@@ -0,0 +1,219 @@
+#if defined(UNICODE) && !defined(_UNICODE)
+#define _UNICODE
+#elif defined(_UNICODE) && !defined(UNICODE)
+#define UNICODE
+#endif
+
+#include <tchar.h>
+#include <windows.h>
+
+/*  Declare Windows procedure  */
+LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+
+class Vector4
+{
+public:
+    double x1;
+    double u1;
+    double x2;
+    double u2;
+    double v[4];
+    Vector4(double a=0,double b=0,double c=0,double d=0)
+    {
+        v[0]=x1=a;
+        v[1]=u1=b;
+        v[2]=x2=c;
+        v[3]=u2=d;
+    }
+    Vector4(const Vector4& v2)
+    {
+        memcpy(v,v2.v,4*sizeof(double));
+    }
+    double&operator[](int index)
+    {
+        return v[index];
+    }
+};
+class Matrix4
+{
+public:
+    double x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15;
+    double m[4][4];
+    Matrix4(double a0=0,double a1=0,double a2=0,double a3=0,double a4=0,double a5=0,double a6=0,double a7=0,double a8=0,double a9=0,double a10=0,double a11=0,double a12=0,double a13=0,double a14=0,double a15=0)
+    {
+        m[0][0]=x0=a0;
+        m[0][1]=x1=a1;
+        m[0][2]=x2=a2;
+        m[0][3]=x3=a3;
+        m[1][0]=x4=a4;
+        m[1][1]=x5=a5;
+        m[1][2]=x6=a6;
+        m[1][3]=x7=a7;
+        m[2][0]=x8=a8;
+        m[2][1]=x9=a9;
+        m[2][2]=x10=a10;
+        m[2][3]=x11=a11;
+        m[3][0]=x12=a12;
+        m[3][1]=x13=a13;
+        m[3][2]=x14=a14;
+        m[3][3]=x15=a15;
+    }
+    Matrix4(const Matrix4& m2)
+    {
+        memcpy(m,m2.m,16*sizeof(double));
+    }
+    double&operator()(int index1,int index2)
+    {
+        return m[index1][index2];
+    }
+};
+Vector4 operator*(Matrix4 m1,Vector4& v1)
+{
+    Vector4 res;
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            res[i]+=(v1[j]*m1(i,j));
+    return res;
+}
+double operator*(Vector4 &v1,Vector4& v2)
+{
+    return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2]+v1[3]*v2[3];
+}
+int Round(double x)
+{
+    return int(x+0.5);
+}
+void DrawHermiteCurve(HDC hdc,double x1,double y1,double u1,double u2,double x2,double y2,double v1,double v2,int n,COLORREF c)
+{
+    double dt=1.0/n;
+    Vector4 Gx(x1,u1,x2,u2);
+    Vector4 Gy(y1,v1,y2,v2);
+    Matrix4 m(1,0,0,0,0,1,0,0,-3,-2,3,-1,2,1,-2,1);
+    Vector4 Vx(m*Gx);
+    Vector4 Vy(m*Gy);
+    double x,y;
+    for(double t=0; t<1; t+=dt)
+    {
+        Vector4 Vt(1,t,t*t,t*t*t);
+        x=Vt*Vx;
+        y=Vt*Vy;
+        SetPixel(hdc,Round(x),Round(y),c);
+
+    }
+}
+void DrawBezierCubicCurve(HDC hdc,double x1,double y1,double x2,double y2,double x3,double y3,double x4,double y4,int n,COLORREF c)
+{
+    double u1=3*(x2-x1);
+    double v1=3*(y2-y1);
+    double u2=3*(x4-x3);
+    double v2=3*(y4-y3);
+    DrawHermiteCurve(hdc,x1,y1,u1,u2,x4,y4,v1,v2,n,c);
+}
+/*  Make the class name into a global variable  */
+TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
+
+int WINAPI WinMain (HINSTANCE hThisInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpszArgument,
+                    int nCmdShow)
+{
+    HWND hwnd;               /* This is the handle for our window */
+    MSG messages;            /* Here messages to the application are saved */
+    WNDCLASSEX wincl;        /* Data structure for the windowclass */
+
+    /* The Window structure */
+    wincl.hInstance = hThisInstance;
+    wincl.lpszClassName = szClassName;
+    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
+    wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
+    wincl.cbSize = sizeof (WNDCLASSEX);
+
+    /* Use default icon and mouse-pointer */
+    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wincl.lpszMenuName = NULL;                 /* No menu */
+    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
+    wincl.cbWndExtra = 0;                      /* structure or the window instance */
+    /* Use Windows's default colour as the background of the window */
+    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+
+    /* Register the window class, and if it fails quit the program */
+    if (!RegisterClassEx (&wincl))
+        return 0;
+
+    /* The class is registered, let's create the program*/
+    hwnd = CreateWindowEx (
+               0,                   /* Extended possibilites for variation */
+               szClassName,         /* Classname */
+               _T("Code::Blocks Template Windows App"),       /* Title Text */
+               WS_OVERLAPPEDWINDOW, /* default window */
+               CW_USEDEFAULT,       /* Windows decides the position */
+               CW_USEDEFAULT,       /* where the window ends up on the screen */
+               544,                 /* The programs width */
+               375,                 /* and height in pixels */
+               HWND_DESKTOP,        /* The window is a child-window to desktop */
+               NULL,                /* No menu */
+               hThisInstance,       /* Program Instance handler */
+               NULL                 /* No Window Creation data */
+           );
+
+    /* Make the window visible on the screen */
+    ShowWindow (hwnd, nCmdShow);
+
+    /* Run the message loop. It will run until GetMessage() returns 0 */
+    while (GetMessage (&messages, NULL, 0, 0))
+    {
+        /* Translate virtual-key messages into character messages */
+        TranslateMessage(&messages);
+        /* Send message to WindowProcedure */
+        DispatchMessage(&messages);
+    }
+
+    /* The program return-value is 0 - The value that PostQuitMessage() gave */
+    return messages.wParam;
+}
+
+
+/*  This function is called by the Windows function DispatchMessage()  */
+double xs,ys,xe,ye,xm1,ym1,xm2,ym2;
+LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)                  /* handle the messages */
+    {
+    case WM_DESTROY:
+        PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+        break;
+    case WM_LBUTTONDOWN:
+    {
+        xs=LOWORD(lParam);
+        ys=HIWORD(lParam);
+        break;
+    }
+    case WM_LBUTTONUP:
+    {
+        xm1=LOWORD(lParam);
+        ym1=HIWORD(lParam);
+        break;
+    }
+    case WM_RBUTTONDOWN:
+    {
+        xm2=LOWORD(lParam);
+        ym2=HIWORD(lParam);
+        break;
+    }
+    case WM_RBUTTONUP:
+    {
+        HDC hdc=GetDC(hwnd);
+        xe=LOWORD(lParam);
+        ye=HIWORD(lParam);
+        DrawBezierCubicCurve(hdc,xs,ys,xm1,ym1,xm2,ym2,xe,ye,120000,RGB(0,0,255));
+        ReleaseDC(hwnd,hdc);
+        break;
+    }
+    default:                      /* for messages that we don't deal with */
+        return DefWindowProc (hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
