9cf443b863791f54b63224c5807a64ad8bede1c1
Sarah Hesham <sarahheshammahmoud@gmail.com>
2022-08-27 22:16:29 +0200

    Add files via upload

---
+++ Graphics Diamond Draw/main.cpp
@@ -0,0 +1,137 @@
+#if defined(UNICODE) && !defined(_UNICODE)
+#define _UNICODE
+#elif defined(_UNICODE) && !defined(UNICODE)
+#define UNICODE
+#endif
+
+#include <tchar.h>
+#include <windows.h>
+
+/*  Declare Windows procedure  */
+LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+
+/*  Make the class name into a global variable  */
+TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
+void Draw4Points(HDC hdc,int xc,int yc,int x,int y)
+{
+    SetPixel(hdc,xc+x,yc+y,RGB(0,0,255));
+    SetPixel(hdc,xc-x,yc+y,RGB(0,0,255));
+    SetPixel(hdc,xc+x,yc-y,RGB(0,0,255));
+    SetPixel(hdc,xc-x,yc-y,RGB(0,0,255));
+}
+void DrawDiamond(HDC hdc,int xc,int yc,int a,int b)
+{
+    int dx =a;
+    int dy = b;
+
+    int twodx = 2 * dx;
+    int twody = 2 * dy;
+
+    int p;
+    int x = a;
+    int y = 0;
+
+    Draw4Points(hdc,xc,yc,x,y);
+
+    p =  twodx - dy;
+    for(y = 1; y <= b ; y++)
+    {
+        if ( p >= 0 )
+        {
+            p +=(twodx - twody);
+            x--;
+        }
+        else
+        {
+            p += twodx;
+        }
+
+        Draw4Points(hdc,xc,yc,x,y);
+    }
+}
+
+
+int WINAPI WinMain (HINSTANCE hThisInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpszArgument,
+                    int nCmdShow)
+{
+    HWND hwnd;               /* This is the handle for our window */
+    MSG messages;            /* Here messages to the application are saved */
+    WNDCLASSEX wincl;        /* Data structure for the windowclass */
+
+    /* The Window structure */
+    wincl.hInstance = hThisInstance;
+    wincl.lpszClassName = szClassName;
+    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
+    wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
+    wincl.cbSize = sizeof (WNDCLASSEX);
+
+    /* Use default icon and mouse-pointer */
+    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wincl.lpszMenuName = NULL;                 /* No menu */
+    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
+    wincl.cbWndExtra = 0;                      /* structure or the window instance */
+    /* Use Windows's default colour as the background of the window */
+    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+
+    /* Register the window class, and if it fails quit the program */
+    if (!RegisterClassEx (&wincl))
+        return 0;
+
+    /* The class is registered, let's create the program*/
+    hwnd = CreateWindowEx (
+               0,                   /* Extended possibilites for variation */
+               szClassName,         /* Classname */
+               _T("Code::Blocks Template Windows App"),       /* Title Text */
+               WS_OVERLAPPEDWINDOW, /* default window */
+               CW_USEDEFAULT,       /* Windows decides the position */
+               CW_USEDEFAULT,       /* where the window ends up on the screen */
+               544,                 /* The programs width */
+               375,                 /* and height in pixels */
+               HWND_DESKTOP,        /* The window is a child-window to desktop */
+               NULL,                /* No menu */
+               hThisInstance,       /* Program Instance handler */
+               NULL                 /* No Window Creation data */
+           );
+
+    /* Make the window visible on the screen */
+    ShowWindow (hwnd, nCmdShow);
+
+    /* Run the message loop. It will run until GetMessage() returns 0 */
+    while (GetMessage (&messages, NULL, 0, 0))
+    {
+        /* Translate virtual-key messages into character messages */
+        TranslateMessage(&messages);
+        /* Send message to WindowProcedure */
+        DispatchMessage(&messages);
+    }
+
+    /* The program return-value is 0 - The value that PostQuitMessage() gave */
+    return messages.wParam;
+}
+
+
+/*  This function is called by the Windows function DispatchMessage()  */
+LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)                  /* handle the messages */
+    {
+    case WM_LBUTTONDBLCLK:
+    {
+        HDC hdc=GetDC(hwnd);
+        DrawDiamond(hdc,880,660,50,80);
+        ReleaseDC(hwnd,hdc);
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+        break;
+    default:                      /* for messages that we don't deal with */
+        return DefWindowProc (hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
